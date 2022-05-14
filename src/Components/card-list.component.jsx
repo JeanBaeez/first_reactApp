abb2ce86e735e61437eaa24c1857e5231d2fd0c4
JeanBaeez <jlbaez44@gmail.com>
2022-05-14 15:48:28 -0400

    adding components

--- src/Components/card-list.component.jsx
+++ src/Components/card-list.component.jsx
@@ -6,25 +6,20 @@ import '../card-style.styles.css';
 
 class CardList extends Component {
 
-
-
-
-
-
-
     render() {
         const { monsters } = this.props;
         return (
             <div className='card-list'>
+
                 {monsters.map(monster => (
 
-                    <div className='card-container'>
+                    <div key={monster.id} className='card-container'>
                         <img src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} alt={`monster ${monster.name}`} />
-                        <h1 className='' key={monster.id}>{monster.name}</h1>
+                        <h2>{monster.name}</h2>
+                        <p >{monster.email}</p>
                     </div>
-
-
-                ))}
+                ))
+                }
             </div>
         )
 
